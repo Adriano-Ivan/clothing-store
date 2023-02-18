f28c40463ae996f64628cbed3d4d4fa2fc426e98
Adriano Ivan de Paula Campos <adrianoivandepaulacampos@gmail.com>
2023-02-18 12:00:58 -0300

    added the addToCourt feature

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,11 +4,12 @@ import { CartContext } from "../../contexts/cart.context";
 import "./cart-icon.styles.scss";
 
 const CartIcon = ({toggleDropdownVisibility}) => {
+    const {numberOfItems} = useContext(CartContext);
 
     return (
         <div onClick={toggleDropdownVisibility}className="cart-icon-container">
             <ShoppingIcon className="shopping-icon"/>
-            <span className="item-count">0</span>
+            <span className="item-count">{numberOfItems()}</span>
         </div>
     )
 }
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,18 +1,56 @@
 import { createContext, useState } from "react";
 
+const addCartItem = (cartItems, productToAdd) => {
+    let cartItemsToDefine = [...cartItems];
+
+    if(!!cartItemsToDefine.find(i => productToAdd.id === i.id)){
+        cartItemsToDefine = cartItemsToDefine.map(i => {
+            if(i.id === productToAdd.id){
+               i.quantity += 1;
+            }
+
+            return i;
+        });
+    } else {
+        productToAdd.quantity = 1;
+        cartItemsToDefine.push(productToAdd);
+    }
+
+    return cartItemsToDefine;
+}
 
 export const CartContext = createContext({
     dropdownCartIsOpen : false,
-    setDropdownCartIsOpen: () => {}
+    setDropdownCartIsOpen: () => {},
+    cartItems: [],
+    addItemToCart: () => {}
 });
 
 export const CartProvider = ({children})=>{
 
     const [dropdownCartIsOpen,setDropdownCartIsOpen] = useState(false);
+    const [cartItems, setCartItems] = useState([]);
+
+    const addItemToCart = (productToAdd) => {
+        setCartItems(addCartItem(cartItems, productToAdd));
+    }
+
+    const numberOfItems = () => {
+
+        return cartItems.reduce((ac, current) => ac + current.quantity, 0);
+    }
+
+    const numberOfProducts = () =>{
+        return cartItems.length;
+    }
 
     const value={
         dropdownCartIsOpen,
-        setDropdownCartIsOpen
+        cartItems,
+        setDropdownCartIsOpen,
+        addItemToCart,
+        numberOfItems,
+        numberOfProducts
     }
 
     return <CartContext.Provider value={value}>
